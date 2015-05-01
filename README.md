9ef8377699a143c0c0769db542330ee5905f2871
John Otander <johnotander@gmail.com>
2015-05-01 15:36:28 -0600

    Add initial functionality

---
+++ README.md
@@ -0,0 +1,40 @@
+# generator-c [![Build Status](https://secure.travis-ci.org/johnotander/generator-c.png?branch=master)](https://travis-ci.org/johnotander/generator-c)
+
+A minimal yeoman generator for CSS modules intended to be published to npm.
+
+This generator prompts you for:
+
+  * Name
+  * Github username
+  * Twitter handle
+  * Package name
+  * Package description
+
+## Installation
+
+```bash
+npm install -g generator-c
+```
+
+## Usage
+
+```bash
+$ mkdir package-name && cd $_
+$ yo c
+```
+
+You will then be prompted for some information that will be used to generate an npm package.
+
+## License
+
+MIT
+
+## Contributing
+
+1. Fork it
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create new Pull Request
+
+Crafted with <3 by [John Otander](http://johnotander.com) ([@4lpine](https://twitter.com/4lpine)).
---
+++ app/index.js
@@ -0,0 +1,60 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var yosay = require('yosay');
+var camelize = require('camelize');
+
+var CGenerator = yeoman.generators.Base.extend({
+  initializing: function () {
+    this.pkg = require('../package.json');
+  },
+
+  prompting: function () {
+    var done = this.async();
+
+    this.log(yosay(
+      'Welcome to the minimal npm package generator'
+    ));
+
+    var prompts = [{
+      name: 'name',
+      message: 'Please enter your name:'
+    }, {
+      name: 'github',
+      message: 'Please enter your github username:'
+    }, {
+      name: 'twitterHandle',
+      message: 'Please enter your twitter username:'
+    }, {
+      name: 'packageName',
+      message: 'Please enter the package name:'
+    }, {
+      name: 'description',
+      message: 'Please enter a package description:'
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.someOption = props.someOption;
+      this.name = props.name;
+      this.github = props.github;
+      this.packageName = props.packageName || 'c-package';
+      this.camelizedPackageName = camelize(props.packageName);
+      this.twitterHandle = props.twitterHandle;
+      this.description = props.description;
+      this.year = (new Date()).getFullYear();
+
+      this.template('_README.md', 'README.md');
+      this.template('_LICENSE.md', 'LICENSE.md');
+      this.template('_package.json', 'package.json');
+      this.template('_index.css', 'index.css');
+
+      this.src.copy('editorconfig', '.editorconfig');
+      this.src.copy('gitignore', '.gitignore');
+
+      done();
+    }.bind(this));
+  }
+});
+
+module.exports = CGenerator;
